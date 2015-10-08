81db970c95b9c68a9c36eeaf3f610217a98a1adf
tikaakusumaa <tikakusuma03@gmail.com>
2015-10-08 08:01:49 +0700

    Tugas warna
    maaf pak terlambat mengumpulkan.
    saya Tika kusuma wardhani
    kelas 2 MI E
    Absen 28

---
+++ Tugas1/WindowsApplication1/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If MyBase.BackColor = Color.Salmon Then
+            MyBase.BackColor = Color.Aqua
+        Else : MyBase.BackColor = Color.Salmon
+        End If
+    End Sub
+End Class
